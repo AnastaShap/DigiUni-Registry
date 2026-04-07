ab8d8ea3a7603e2466b7a9b4623358cdb90551b9
Anastasiia Shapoval <nastya030207@gmail.com>
2026-04-07 18:25:10 +0300

    fix: InMemoryTeacherRepository and InMemoryStudentRepository

--- src/main/java/ua/university/repository/InMemoryTeacherRepository.java
+++ src/main/java/ua/university/repository/InMemoryTeacherRepository.java
@@ -1,8 +1,14 @@
 package ua.university.repository;
 
+import ua.university.domain.Faculty;
 import ua.university.domain.Student;
 import ua.university.domain.Teacher;
 
-public class InMemoryTeacherRepository extends InMemoryRepository<Teacher, String> {
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
+public class InMemoryTeacherRepository extends InMemoryRepository<Teacher, String> {
+    public void save(Teacher teacher) {
+        storage.put(teacher.getId(), teacher);
+    }
 }
