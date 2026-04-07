92b0a81f1405dc87b3dd25e5b47c71f83d4c4f8a
Anastasiia Shapoval <nastya030207@gmail.com>
2026-04-07 18:25:55 +0300

    feat: logging Improvement (using Log4j2 now)

--- src/main/java/ua/university/Main.java
+++ src/main/java/ua/university/Main.java
@@ -1,12 +1,12 @@
 package ua.university;
 import ua.university.ui.MainMenu;
-import ua.university.util.ConsoleLogger;
+import ua.university.util.Log4j2Logger;
+
 public class Main {
     public static void main(String[] args) {
 
         // Menu test
-        MainMenu mainMenu = new MainMenu(new ConsoleLogger());
-
+        MainMenu mainMenu = new MainMenu(new Log4j2Logger());
         mainMenu.run();
     }
 }
---
+++ src/main/java/ua/university/util/Log4j2Logger.java
@@ -0,0 +1,18 @@
+package ua.university.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Log4j2Logger implements ILogger {
+    private static final Logger logger = LoggerFactory.getLogger("DigiUni");
+
+    @Override
+    public void info(String message) {
+        logger.info(message);
+    }
+
+    // Додаємо метод для логування помилок (знадобиться для Exception handling)
+    public void error(String message, Throwable t) {
+        logger.error(message, t);
+    }
+}
